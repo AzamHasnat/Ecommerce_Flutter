d4adb7ec672acf6a81f5a47f2f1f3e610d5fab8c
AzamHasnat <hasnat.azam@g.bracu.ac.bd>
2024-04-09 22:14:34 +0600

    work on user_cubit

--- lib/logic/cubits/user_cubit/user_cubit.dart
+++ lib/logic/cubits/user_cubit/user_cubit.dart
@@ -1,6 +1,30 @@
+import 'package:ecommerce_flutter/data/models/user/user_model.dart';
 import 'package:ecommerce_flutter/logic/cubits/user_cubit/user_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../data/repositories/user_repository.dart';
+
 class UserCubit extends Cubit<UserState> {
   UserCubit() : super(UserInitialState());
+  final UserRepository _userRepository = UserRepository();
+
+  void signIn({required String email, required String password}) async {
+    try {
+      UserModel userModel =
+          await _userRepository.signIn(email: email, password: password);
+      emit(UserLoggedInState(userModel));
+    } catch (ex) {
+      emit(UserErrorState(ex.toString()));
+    }
+  }
+
+   void createAccount({required String email, required String password}) async {
+    try {
+      UserModel userModel =
+          await _userRepository.signIn(email: email, password: password);
+      emit(UserLoggedInState(userModel));
+    } catch (ex) {
+      emit(UserErrorState(ex.toString()));
+    }
+  }
 }
